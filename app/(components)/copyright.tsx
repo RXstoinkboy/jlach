f17c9ecd709e9185a8f74feafe2160a1693f2d75
RXstoinkboy <erykludwin@gmail.com>
2025-06-06 00:04:21 +0200

    adjust copyright section

--- app/(components)/copyright.tsx
+++ app/(components)/copyright.tsx
@@ -1,7 +1,10 @@
+import { Separator } from "@/components/ui/separator";
+
 export const Copyright = () => {
   return (
-    <section className="flex justify-between items-center w-full">
+    <section className="flex justify-end items-center w-full gap-3 text-neutral-400">
       <p>Copyright {new Date().getFullYear()}</p>
+      <Separator orientation="vertical" />
       <p>
         <a href={process.env.NEXT_PUBLIC_BUILT_BY_URL} target="_blank">
           Built by Eryk Ludwin
