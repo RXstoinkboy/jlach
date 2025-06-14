f96a3b77d4cae1e84cb8d865d5419e26c0502175
RXstoinkboy <erykludwin@gmail.com>
2025-06-14 23:35:48 +0200

    fix navigation

--- app/(components)/navigation/options.ts
+++ app/(components)/navigation/options.ts
@@ -1,6 +1,8 @@
 export const menuItems = [
     { href: "/#home", label: "Start" },
-    { href: "/#about", label: "O mnie" },
+    { href: "/#why-me", label: "Wartości" },
     { href: "/#offer", label: "Oferta" },
+    { href: "/#cooperation", label: "Współpraca" },
+    { href: "/#about", label: "O mnie" },
     { href: "/#contact", label: "Kontakt" },
   ];
