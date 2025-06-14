1d5241a76347bee6a3167fdb6d0e200c55ba2484
RXstoinkboy <erykludwin@gmail.com>
2025-06-15 01:14:51 +0200

    improve font colors

--- app/(hero)/hero.tsx
+++ app/(hero)/hero.tsx
@@ -10,15 +10,16 @@ export const Hero = () => {
   return (
     <section className="flex min-h-screen w-full justify-center items-center bg-accent pt-20">
       <div className="flex flex-1 w-full max-w-7xl items-center flex-col md:flex-row gap-14 sm:gap-0">
-        <article className="flex flex-1 flex-col gap-2 max-w-6xl justify-start p-4 pt-24 sm:pt-4">
-          <h2 className="text-2xl font-semibold text-center sm:text-left">
+        <article className="flex flex-1 flex-col gap-6 max-w-6xl justify-start p-4 pt-24 sm:pt-4">
+          <h2 className="text-xl font-semibold text-center sm:text-left text-muted-foreground">
             Twój agent ubezpieczeniowy
           </h2>
-          <h1 className="text-7xl font-black text-center sm:text-left">
+
+          <h1 className="text-8xl font-black text-center sm:text-left">
             {process.env.NEXT_PUBLIC_FULL_NAME}
           </h1>
 
-          <h2 className="text-xl font-normal text-neutral-500 text-center sm:text-left">
+          <h2 className="text-xl font-normal text-muted-foreground text-center sm:text-left">
             Pomagam wybrać najlepsze ubezpieczenia. <br />
             Bez stresu, bez drobnego druku.
           </h2>
