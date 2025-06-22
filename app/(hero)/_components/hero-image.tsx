bbc9e889e43706143e5962efbe83f6e255789481
RXstoinkboy <erykludwin@gmail.com>
2025-06-23 00:18:34 +0200

    animate hero image

--- app/(hero)/_components/hero-image.tsx
+++ app/(hero)/_components/hero-image.tsx
@@ -1,10 +1,43 @@
 import Image from "next/image";
 import Photo from "@/public/hero.jpg";
 import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
+import { motion } from "motion/react";
 
 export const HeroImage = ({ className }: { className?: string }) => {
+  const [loaded, setLoaded] = useState(false);
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsDesktop(window.matchMedia("(min-width: 768px)").matches);
+    };
+
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
   return (
-    <div
+    <motion.div
+      initial={{
+        opacity: 0,
+        scale: 1.05,
+        y: 20,
+        filter: "blur(5px)",
+      }}
+      animate={{
+        opacity: loaded ? 1 : 0,
+        scale: 1,
+        y: 0,
+        filter: "blur(0px)",
+      }}
+      transition={{
+        duration: 0.7,
+        ease: "easeInOut",
+        delay: isDesktop ? 0.5 : 0,
+      }}
       className={cn("h-full relative rounded-2xl overflow-hidden", className)}
     >
       <Image
@@ -12,8 +45,9 @@ export const HeroImage = ({ className }: { className?: string }) => {
         placeholder="blur"
         src={Photo}
         alt="Hero Image"
+        onLoadingComplete={() => setLoaded(true)}
         className="w-full h-full object-cover object-top md:object-center"
       />
-    </div>
+    </motion.div>
   );
 };
