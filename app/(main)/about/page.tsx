952b1e40083b8b133ae51a73be5d8efac559725f
RXstoinkboy <erykludwin@gmail.com>
2025-07-06 01:00:35 +0200

    improve separator

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -1,46 +1,50 @@
 "use client";
 
 import { InfoSection } from "@/app/_components/info-section";
+import { Separator } from "@/components/ui/separator";
 import { useRouter } from "next/navigation";
 
 export default function About() {
   const router = useRouter();
 
   return (
-    <InfoSection id="about" className="pt-24">
-      <InfoSection.Title>O mnie</InfoSection.Title>
-      <InfoSection.Subtitle>
-        Cześć, jestem Jerzy – agent, który naprawdę słucha.
-      </InfoSection.Subtitle>
-      <InfoSection.Body>
-        <InfoSection.Content
-          onClick={() => router.back()}
-          actionButtonText="Powrót do strony głównej"
-        >
-          <div className="flex flex-col gap-4">
-            <p>
-              Od ponad 20 lat pomagam ludziom i rodzinom czuć się bezpieczniej.
-              Działam uczciwie, tłumaczę w prostych słowach i doradzam, jakbym
-              ubezpieczał swoich bliskich.
-            </p>
-            <p>
-              Od ponad 20 lat pomagam ludziom i rodzinom czuć się bezpieczniej.
-              Działam uczciwie, tłumaczę w prostych słowach i doradzam, jakbym
-              ubezpieczał swoich bliskich.
-            </p>
-            <p>
-              Od ponad 20 lat pomagam ludziom i rodzinom czuć się bezpieczniej.
-              Działam uczciwie, tłumaczę w prostych słowach i doradzam, jakbym
-              ubezpieczał swoich bliskich.
-            </p>
-            <p>
-              Od ponad 20 lat pomagam ludziom i rodzinom czuć się bezpieczniej.
-              Działam uczciwie, tłumaczę w prostych słowach i doradzam, jakbym
-              ubezpieczał swoich bliskich.
-            </p>
-          </div>
-        </InfoSection.Content>
-      </InfoSection.Body>
-    </InfoSection>
+    <>
+      <InfoSection id="about" className="pt-24">
+        <InfoSection.Title>O mnie</InfoSection.Title>
+        <InfoSection.Subtitle>
+          Cześć, jestem Jerzy – agent, który naprawdę słucha.
+        </InfoSection.Subtitle>
+        <InfoSection.Body>
+          <InfoSection.Content
+            onClick={() => router.back()}
+            actionButtonText="Powrót do strony głównej"
+          >
+            <div className="flex flex-col gap-4">
+              <p>
+                Od ponad 20 lat pomagam ludziom i rodzinom czuć się
+                bezpieczniej. Działam uczciwie, tłumaczę w prostych słowach i
+                doradzam, jakbym ubezpieczał swoich bliskich.
+              </p>
+              <p>
+                Od ponad 20 lat pomagam ludziom i rodzinom czuć się
+                bezpieczniej. Działam uczciwie, tłumaczę w prostych słowach i
+                doradzam, jakbym ubezpieczał swoich bliskich.
+              </p>
+              <p>
+                Od ponad 20 lat pomagam ludziom i rodzinom czuć się
+                bezpieczniej. Działam uczciwie, tłumaczę w prostych słowach i
+                doradzam, jakbym ubezpieczał swoich bliskich.
+              </p>
+              <p>
+                Od ponad 20 lat pomagam ludziom i rodzinom czuć się
+                bezpieczniej. Działam uczciwie, tłumaczę w prostych słowach i
+                doradzam, jakbym ubezpieczał swoich bliskich.
+              </p>
+            </div>
+          </InfoSection.Content>
+        </InfoSection.Body>
+      </InfoSection>
+      <Separator className="max-w-10/12 mx-auto mb-8" />
+    </>
   );
 }
